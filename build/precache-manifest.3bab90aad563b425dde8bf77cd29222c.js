7b7f3735150283733a7e4a7a4737da7509085994
Brian <bomalley3@live.esu.edu>
2020-02-23 17:36:06 -0500

    Adding updated files

---
+++ build/precache-manifest.3bab90aad563b425dde8bf77cd29222c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4574c0d52525e7b0719432b8a61d88d3",
+    "url": "/Peaceable-Queens-GUI/index.html"
+  },
+  {
+    "revision": "d48751ef98e41cee8593",
+    "url": "/Peaceable-Queens-GUI/static/css/main.d3324456.chunk.css"
+  },
+  {
+    "revision": "db06b3eef14e45f8863d",
+    "url": "/Peaceable-Queens-GUI/static/js/2.9d7269bd.chunk.js"
+  },
+  {
+    "revision": "d48751ef98e41cee8593",
+    "url": "/Peaceable-Queens-GUI/static/js/main.c563b545.chunk.js"
+  },
+  {
+    "revision": "02726a23ecf54dba07ca",
+    "url": "/Peaceable-Queens-GUI/static/js/runtime~main.2f0e8dc7.js"
+  }
+]);
